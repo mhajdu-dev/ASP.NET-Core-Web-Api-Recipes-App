ad4b5dbe89b9080289f87e3cdd7e3ee747aa25b0
Marci <hajdumarcel@gmail.com>
2023-07-22 18:34:16 +0200

    DbContext created

---
+++ Recipes.API/Data/RecipesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recipes.API.Models.Domain;
+
+namespace Recipes.API.Data
+{
+    public class RecipesDbContext: DbContext
+    {
+        public RecipesDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+                        
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<TypeOfDish> TypeOfDishes { get; set; }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
